eb78664ad21640cd787053727fbbfd13e6d3dee5
Nilesh3524 <nileshnarule24@gmail.com>
2024-12-04 12:04:12 +0530

    admin serviece

--- src/main/java/com/main/ecommerce/services/AdminServices.java
+++ src/main/java/com/main/ecommerce/services/AdminServices.java
@@ -1,6 +1,18 @@
 package com.main.ecommerce.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.main.ecommerce.entities.Admin;
+import com.main.ecommerce.entities.Product;
+
+@Service
 public interface AdminServices {
 
+    Optional<Admin> getAdminById(long id);
+
+    Optional<List<Product>> getProductsByAdmin(Admin admin);
     
 }
